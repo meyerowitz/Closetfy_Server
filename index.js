676c6a3457ec50c308e6bd6927db5f7e6276925f
Rebeca <meyerowitzrebeca@gmail.com>
2025-10-01 23:34:20 +0200

    -

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ router.get("/articulos", async (req, res) => {
             // Si no hay artículos, renderiza la vista con una lista vacía
             console.log("No se encontraron artículos en la base de datos.");
             return res.render('articulos', { 
-                articulos: rows, // Lista vacía para que el EJS muestre el placeholder
+                articulos: [], // Lista vacía para que el EJS muestre el placeholder
                 title: "Inventario de Artículos (0 Encontrados)"
             });
         }
@@ -112,7 +112,7 @@ router.get("/articulos", async (req, res) => {
         console.log(`Se encontraron ${rows.length} artículos.`);
         
         res.status(200).render('articulos', {
-            //articulos: rows, 
+            articulos: rows, 
             title: `Inventario de Artículos (${rows.length})`
         });
 
